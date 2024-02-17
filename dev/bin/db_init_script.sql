11018425ce1bcd9d6361631be1505f54fe63b7c8
Calum Lindsay <calumlindsay.434c@gmail.com>
2024-02-17 15:12:21 +0000

    Overhaul of db_init_script
    Refactoring:
    - Removed excess boilerplate created by MySQL Workbench.
    - Removed redundant NULL keyword from column definitions where NULL is allowed.
    - Drop schema and recreate to allow tables to be changed easily.
    - Remove now obsolete IF NOT EXISTS statements.
    
    Problems Fixed:
    - Removed all instances of VISIBLE keyword as MariaDB doesn't support this.
    - Added missing days to Scedule.days enum.
    - Removed NOT NULL from user.House_houseId as new users will not belong to a house.
    - Primary and Foreign keys fixed and simplified.
    - Added startTime and stopTime to Task_has_user.
    
    Questions:
    - Why are startTime, endTime VARCHAR's? just how timestamp works?
    - Removing INDEX statements would make the script a bit more readable but I think they are supposed to improve performance..?
    
    Possible problems:
    - If a task within a room needs to be done by a user more than once in a given rota cycle we will need to change the primary key for Task_has_user (E.g. Jane hoovers on Monday and Friday at 3pm).

--- dev/bin/db_init_script.sql
+++ dev/bin/db_init_script.sql
@@ -1,193 +1,156 @@
--- MySQL Workbench Forward Engineering
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+DROP SCHEMA IF EXISTS `mydb`;
+CREATE SCHEMA `mydb` DEFAULT CHARACTER SET utf8;
+USE `mydb`;
 
 -- -----------------------------------------------------
 -- Table `mydb`.`House`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`House` (
+CREATE TABLE `mydb`.`House` (
   `houseId` INT NOT NULL,
-  `roomCounter` INT NULL,
-  PRIMARY KEY (`houseId`))
-ENGINE = InnoDB;
+  `roomCounter` INT,
+  PRIMARY KEY (`houseId`));
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`user`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`user` (
+CREATE TABLE `mydb`.`user` (
   `forename` VARCHAR(45) NOT NULL,
   `surname` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
   `role` TINYINT NOT NULL DEFAULT 0,
-  `House_houseId` INT NOT NULL,
-  `personalPoints` INT NULL,
-  PRIMARY KEY (`email`, `House_houseId`),
-  INDEX `fk_user_House_idx` (`House_houseId` ASC) VISIBLE,
+  `House_houseId` INT,
+  `personalPoints` INT,
+  PRIMARY KEY (`email`),
+  INDEX `fk_user_House_idx` (`House_houseId` ASC),
   CONSTRAINT `fk_user_House`
     FOREIGN KEY (`House_houseId`)
-    REFERENCES `mydb`.`House` (`houseId`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `mydb`.`House` (`houseId`));
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`Room`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Room` (
+CREATE TABLE `mydb`.`Room` (
   `roomId` INT NOT NULL,
-  `name` VARCHAR(45) NULL,
+  `name` VARCHAR(45),
   `houseId` INT NOT NULL,
-  PRIMARY KEY (`roomId`, `houseId`),
-  INDEX `fk_Room_House1_idx` (`houseId` ASC) VISIBLE,
+  PRIMARY KEY (`roomId`),
+  INDEX `fk_Room_House1_idx` (`houseId` ASC),
   CONSTRAINT `fk_Room_House1`
     FOREIGN KEY (`houseId`)
-    REFERENCES `mydb`.`House` (`houseId`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `mydb`.`House` (`houseId`));
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`Task`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Task` (
+CREATE TABLE `mydb`.`Task` (
   `taskId` INT NOT NULL,
   `name` VARCHAR(45) NOT NULL,
-  `description` VARCHAR(1000) NULL,
+  `description` VARCHAR(1000),
   `Room_roomId` INT NOT NULL,
   `Room_houseId` INT NOT NULL,
-  PRIMARY KEY (`taskId`, `Room_roomId`, `Room_houseId`),
-  INDEX `fk_Task_Room1_idx` (`Room_roomId` ASC, `Room_houseId` ASC) VISIBLE,
+  PRIMARY KEY (`taskId`),
+  INDEX `fk_Task_Room1_idx` (`Room_roomId` ASC),
   CONSTRAINT `fk_Task_Room1`
-    FOREIGN KEY (`Room_roomId` , `Room_houseId`)
-    REFERENCES `mydb`.`Room` (`roomId` , `houseId`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    FOREIGN KEY (`Room_roomId`)
+    REFERENCES `mydb`.`Room` (`roomId`));
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`Rule`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Rule` (
+CREATE TABLE `mydb`.`Rule` (
   `ruleId` INT NOT NULL,
   `startTime` DATETIME NOT NULL,
   `endTime` DATETIME NOT NULL,
   `user_email` VARCHAR(45) NOT NULL,
-  `houseId` INT NOT NULL,
   `taskId` INT NOT NULL,
   `roomId` INT NOT NULL,
-  PRIMARY KEY (`ruleId`, `user_email`, `houseId`, `taskId`, `roomId`),
-  INDEX `fk_Rule_user1_idx` (`user_email` ASC, `houseId` ASC) VISIBLE,
-  INDEX `fk_Rule_Task1_idx` (`taskId` ASC, `roomId` ASC) VISIBLE,
+  PRIMARY KEY (`ruleId`),
+  INDEX `fk_Rule_user1_idx` (`user_email` ASC),
+  INDEX `fk_Rule_Task1_idx` (`taskId` ASC),
   CONSTRAINT `fk_Rule_user1`
-    FOREIGN KEY (`user_email` , `houseId`)
-    REFERENCES `mydb`.`user` (`email` , `House_houseId`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    FOREIGN KEY (`user_email`)
+    REFERENCES `mydb`.`user` (`email`),
   CONSTRAINT `fk_Rule_Task1`
-    FOREIGN KEY (`taskId` , `roomId`)
-    REFERENCES `mydb`.`Task` (`taskId` , `Room_roomId`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    FOREIGN KEY (`taskId`)
+    REFERENCES `mydb`.`Task` (`taskId`));
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`Schedule`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Schedule` (
+CREATE TABLE `mydb`.`Schedule` (
   `scheduleId` INT NOT NULL,
-  `startTime` VARCHAR(45) NULL,
-  `endTime` VARCHAR(45) NULL,
-  `days` ENUM('Monday', 'Tuesday') NULL,
-  `Schedulecol` VARCHAR(45) NULL,
+  `startTime` VARCHAR(45),
+  `endTime` VARCHAR(45),
+  `days` ENUM('Monday', 'Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday') NULL,
+  `Schedulecol` VARCHAR(45),
   `user_email` VARCHAR(45) NOT NULL,
-  `user_House_houseId` INT NOT NULL,
-  PRIMARY KEY (`scheduleId`, `user_email`, `user_House_houseId`),
-  INDEX `fk_Schedule_user1_idx` (`user_email` ASC, `user_House_houseId` ASC) VISIBLE,
+  PRIMARY KEY (`scheduleId`),
+  INDEX `fk_Schedule_user1_idx` (`user_email` ASC),
   CONSTRAINT `fk_Schedule_user1`
-    FOREIGN KEY (`user_email` , `user_House_houseId`)
-    REFERENCES `mydb`.`user` (`email` , `House_houseId`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    FOREIGN KEY (`user_email`)
+    REFERENCES `mydb`.`user` (`email`));
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`taskPoints`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`taskPoints` (
+CREATE TABLE `mydb`.`taskPoints` (
   `pointId` INT NOT NULL,
   `quantity` INT NOT NULL,
   `Task_taskId` INT NOT NULL,
-  `Task_Room_roomId` INT NOT NULL,
-  `Task_Room_houseId` INT NOT NULL,
-  PRIMARY KEY (`pointId`, `Task_taskId`, `Task_Room_roomId`, `Task_Room_houseId`),
-  INDEX `fk_taskPoints_Task1_idx` (`Task_taskId` ASC, `Task_Room_roomId` ASC, `Task_Room_houseId` ASC) VISIBLE,
-  CONSTRAINT `fk_taskPoints_Task1`
-    FOREIGN KEY (`Task_taskId` , `Task_Room_roomId` , `Task_Room_houseId`)
-    REFERENCES `mydb`.`Task` (`taskId` , `Room_roomId` , `Room_houseId`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  `Room_roomId` INT NOT NULL,
+  PRIMARY KEY (`pointId`),
+  INDEX `fk_Rule_task2_idx` (`Task_taskId` ASC),
+  INDEX `fk_Rule_room2_idx` (`Room_roomId` ASC),
+  CONSTRAINT `fk_Rule_task2`
+    FOREIGN KEY (`Task_taskId`)
+    REFERENCES `mydb`.`Task` (`taskId`),
+  CONSTRAINT `fk_Rule_room2`
+    FOREIGN KEY (`Room_roomId`)
+    REFERENCES `mydb`.`Room` (`roomId`));
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`Rota`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Rota` (
+CREATE TABLE `mydb`.`Rota` (
   `rotaId` INT NOT NULL,
-  PRIMARY KEY (`rotaId`))
-ENGINE = InnoDB;
+  PRIMARY KEY (`rotaId`));
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`Task_has_user`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Task_has_user` (
+CREATE TABLE `mydb`.`Task_has_user` (
   `taskId` INT NOT NULL,
   `roomId` INT NOT NULL,
   `email` VARCHAR(45) NOT NULL,
-  `houseId` INT NOT NULL,
-  `Rota_rotaId` INT NOT NULL,
-  PRIMARY KEY (`taskId`, `roomId`, `email`, `houseId`, `Rota_rotaId`),
-  INDEX `fk_Task_has_user_user1_idx` (`email` ASC, `houseId` ASC) VISIBLE,
-  INDEX `fk_Task_has_user_Task1_idx` (`taskId` ASC, `roomId` ASC) VISIBLE,
-  INDEX `fk_Task_has_user_Rota1_idx` (`Rota_rotaId` ASC) VISIBLE,
+  `rotaId` INT NOT NULL,
+  `startTime` VARCHAR(45) NOT NULL,
+  `endTime` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`taskId`, `roomId`, `email`, `rotaId`),
+  INDEX `fk_Task_has_user_user1_idx` (`email` ASC),
+  INDEX `fk_Task_has_user_Task1_idx` (`taskId` ASC),
+  INDEX `fk_Task_has_user_Room1_idx` (`roomId` ASC),
+  INDEX `fk_Task_has_user_Rota1_idx` (`rotaId` ASC),
   CONSTRAINT `fk_Task_has_user_Task1`
-    FOREIGN KEY (`taskId` , `roomId`)
-    REFERENCES `mydb`.`Task` (`taskId` , `Room_roomId`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    FOREIGN KEY (`taskId`)
+    REFERENCES `mydb`.`Task` (`taskId`),
+  CONSTRAINT `fk_Task_has_user_Room1`
+    FOREIGN KEY (`roomId`)
+    REFERENCES `mydb`.`Room` (`roomId`),
   CONSTRAINT `fk_Task_has_user_user1`
-    FOREIGN KEY (`email` , `houseId`)
-    REFERENCES `mydb`.`user` (`email` , `House_houseId`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    FOREIGN KEY (`email`)
+    REFERENCES `mydb`.`user` (`email`),
   CONSTRAINT `fk_Task_has_user_Rota1`
-    FOREIGN KEY (`Rota_rotaId`)
-    REFERENCES `mydb`.`Rota` (`rotaId`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+    FOREIGN KEY (`rotaId`)
+    REFERENCES `mydb`.`Rota` (`rotaId`));
+    

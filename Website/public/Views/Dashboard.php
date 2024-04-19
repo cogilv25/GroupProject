bb5332cded35f1f51190317dfae5ab9fa64aec3f
Jack R Scaife <19008831@uhi.ac.uk>
2024-04-19 16:37:20 +0100

    Updated dashboard sidebar
    Made sidebar for dashboard and included it into dashboard

--- Website/public/Views/Dashboard.php
+++ Website/public/Views/Dashboard.php
@@ -16,43 +16,8 @@
     <script src="https://cdn.jsdelivr.net/npm/foundation-sites@6.8.1/dist/js/foundation.min.js" crossorigin="anonymous"></script>    
 </head>
 <body>
-<div class="off-canvas-wrapper">
-   <div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
-        <div class="off-canvas position-left reveal-for-large" id="sidenav" data-off-canvas data-position="left">
-            <div class="grid-x grid-padding-x">
-                <div class="cell small-12 text-center">
-                    <a id="logo">CleanSync</a>
-                </div>
-                <div class="cell">
-                    <ul class="vertical menu ">
-                      <li><a class="navlink" href="/">Home</a></li>
-                      <li><a class="navlink" href="/notifications">Notifications</a></li>
-                      <li>
-                            <a class="navlink">Rules</a>
-                            <ul class="menu vertical nested">
-                                <li><a class="navlink" href="/rule">View All Rules</a></li>
-                                <li><a class="navlink" href="/rule/create">Add Rule</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                      <li><a class="navlink" href="/task">Tasks</a></li>
-                      <li><a class="navlink" href="/household">HouseHold</a></li>
-                      <li><a class="navlink" href="/schedule">Schedule</a></li>
-                      <li><a class="navlink" href="/room">Rooms</a></li>
-                      <li><a class="navlink" href="/profile">Profile</a></li>
-                      <li><a class="navlink" href="/settings">Settings</a></li>
-                      <li><a class="navlink" href="/logout">Logout</a></li>
-                  </ul>
-                </div>
-            </div>
-        </div>
-        <div class="off-canvas-content" data-off-canvas-content>
-            <div class="title-bar hide-for-large">
-                <div class="title-bar-left">
-                    <button class="menu-icon" type="button" data-open="sidenav"></button>
-                    <span class="title-bar-title">CleanSync</span>
-                </div>
-            </div>
+<?php include 'sidebar.php'; ?>
+
             <div id="main-content" class="grid-x grid-margin-x small-up-1 medium-up-3 large-up-4">
             <?php 
                 if(isset($page))
@@ -72,7 +37,10 @@
         </div>
     </div>
 </div>
+<script> 
+    $(document).foundation();
 
+</script>
 
 
 
